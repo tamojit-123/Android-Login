c60654ce0b1f7a9db9539e89479ee6226f82fa4b
tamojit-123 <tamo.das.97@gmail.com>
2019-04-26 21:55:58 +0530

    Initial commit

---
+++ app/src/main/java/com/androidcodefinder/loginscreendemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.androidcodefinder.loginscreendemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin,btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin=(Button)findViewById(R.id.btnLogin);
+        btnSignup=(Button)findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/androidcodefinder/loginscreendemo/SignUpActivity.java
@@ -0,0 +1,28 @@
+package com.androidcodefinder.loginscreendemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private Button btnLogin,btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        btnLogin=(Button)findViewById(R.id.btnLogin);
+        btnSignup=(Button)findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
